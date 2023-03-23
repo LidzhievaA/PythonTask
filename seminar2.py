bfd0a27e8b589f56c2cd02214caa23d80811b9c7
Lidzhieva <magadieva.alsu@yandex.ru>
2023-03-23 15:04:37 +0500

    Семинар 2

--- seminar2.py
+++ seminar2.py
@@ -102,3 +102,22 @@
 # Пользователь вводит одно число N – количество арбузов. Вторая строка содержит N чисел, 
 # записанных на новой строчке каждое. Здесь каждое число – это масса соответствующего арбуза
 
+# quantity = int(input("Количество арбузов: "))
+# max_weight = 0
+# min_weight = 0
+# for i in range(quantity):
+# current_weight = int(input("Вес арбуза: "))
+# while current_weight <= 0 or current_weight > 30000:
+# print("Такой арбуз не унести")
+# current_weight = int(input("Вес арбуза: "))
+# if i == 0:
+# min_weight = current_weight
+# max_weight = current_weight
+# if i != 0:
+# if current_weight > max_weight and current_weight < 30000:
+# max_weight = current_weight
+# if current_weight < min_weight and current_weight > 0:
+# min_weight = current_weight
+
+# print(f"Арбуз для себя {max_weight}")
+# print(f"Арбуз для тещи {min_weight}")
